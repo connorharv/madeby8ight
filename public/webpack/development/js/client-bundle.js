5b3b04718752c558fd2f06c609f81ed4ff1b64c6
connorharv <connorharvey803@gmail.com>
2025-04-30 21:48:25 -0600

    Added basic about and contact page stuff

--- public/webpack/development/js/client-bundle.js
+++ public/webpack/development/js/client-bundle.js
@@ -12,7 +12,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-var _jsxFileName = "/mnt/c/Users/Connor/Desktop/Made By 8ight/madeby8ight/client/app/components/HelloWorld.jsx";
+var _jsxFileName = "/mnt/c/Users/conbo/OneDrive/Desktop/madeby8ight/client/app/components/HelloWorld.jsx";
 
 
 
@@ -93,7 +93,7 @@ class HelloWorld extends (react__WEBPACK_IMPORTED_MODULE_0___default().Component
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-var _jsxFileName = "/mnt/c/Users/Connor/Desktop/Made By 8ight/madeby8ight/client/app/components/Test.jsx";
+var _jsxFileName = "/mnt/c/Users/conbo/OneDrive/Desktop/madeby8ight/client/app/components/Test.jsx";
 
 const Test = () => {
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
